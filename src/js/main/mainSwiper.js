b38b6292438c1ddf70893f10bcf381d65a542ea9
gkstkdgkr <gkstkdgkr12@gmail.com>
2024-07-10 18:05:43 +0900

    edit : mainpages edit

---
+++ src/js/main/mainSwiper.js
@@ -0,0 +1,155 @@
+const swiper_case1 = new Swiper('.swiper-container__case1', {
+  slidesPerView: 7.3, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 3.2,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 3.2,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 5.2,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 7.2,
+    },
+  },
+});
+
+const swiper_case2 = new Swiper('.swiper-container__case2', {
+  slidesPerView: 5.1, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 2.1,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 2.1,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 3.1,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 5.1,
+    },
+  },
+});
+
+const swiper_case3 = new Swiper('.swiper-container__case3', {
+  slidesPerView: 7.3, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 3.2,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 3.2,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 5.2,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 7.2,
+    },
+  },
+});
+
+const swiper_case4 = new Swiper('.swiper-container__case4', {
+  slidesPerView: 5.1, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 2.1,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 2.1,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 3.1,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 5.1,
+    },
+  },
+});
+
+const swiper_case5 = new Swiper('.swiper-container__case5', {
+  slidesPerView: 5.1, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 2.1,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 2.1,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 4.1,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 6.1,
+    },
+  },
+});
+
+const swiper_case6 = new Swiper('.swiper-container__case6', {
+  slidesPerView: 5.1, // 기본적으로 큰 화면에서 보여지는 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 간의 간격
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1: {
+      slidesPerView: 2.1,
+    },
+    // 320px 이하일 때
+    320: {
+      slidesPerView: 2.1,
+    },
+    // 321px 이상 768px 이하일 때
+    321: {
+      slidesPerView: 3.1,
+    },
+    // 769px 이상일 때
+    769: {
+      slidesPerView: 5.1,
+    },
+  },
+});
