72770a9af07307c9c3945aa1419e5c31da7dd466
vngkt3 <wkdalsgur456@naver.com>
2024-07-17 11:26:13 +0900

    Feat: 아이디, 비밀번호 공란 확인, 회원가입 페이지 이동 추가

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -11,33 +11,35 @@ loginBtn.addEventListener('click', (event) => {
   const username = loginId.value;
   const password = loginPw.value;
 
-  pb.collection('users')
-    .authWithPassword(username, password)
-    .then(
-      async () => {
-        const { model, token } = await getStorage('pocketbase_auth');
-        setStorage('auth', {
-          isAuth: !!model,
-          user: model,
-          token,
-        });
-        alert('로그인 완료! 메인페이지로 이동합니다.');
-        location.href = '/index.html';
-      },
-      () => {
-        alert('인증된 사용자가 아닙니다.');
-      }
-    );
+  if (!username) {
+    alert('아이디를 입력해 주세요!');
+    return;
+  } else if (!password) {
+    alert('비밀번호를 입력해 주세요!');
+  } else {
+    pb.collection('users')
+      .authWithPassword(username, password)
+      .then(
+        async () => {
+          const { model, token } = await getStorage('pocketbase_auth');
+          setStorage('auth', {
+            isAuth: model,
+            user: model,
+            token,
+          });
+          alert('로그인 완료! 메인페이지로 이동합니다.');
+          location.href = '/index.html';
+        },
+        () => {
+          alert('인증된 사용자가 아닙니다.');
 
-  // if (username === '') {
-  //   alert('아이디를 입력해 주세요.');
-  // } else if (password === '') {
-  //   alert('비밀번호를 입력해 주세요.');
-  // } else if (username === 'user' && password === '123') {
-  //   alert('로그인 성공!');
-  //   window.location.href = 'https://sorry6.netlify.app';
-  // } else {
-  //   alert('잘못된 아이디, 비밀번호를 입력했습니다.');
-  //   location.reload();
-  // }
+          const pass = confirm('회원가입 페이지로 이동하시겠습니까?');
+          if (pass === true) {
+            location.href = '/src/pages/signup/signUp.html';
+          } else {
+            location.reload();
+          }
+        }
+      );
+  }
 });
